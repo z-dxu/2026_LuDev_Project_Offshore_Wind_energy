a4454f15d7a8bee47c17fca2c726f5dd4f309dbf
Huss-Alamodi <56467257+Huss-Alamodi@users.noreply.github.com>
2026-03-13 15:25:46 +0100

    Fixed linting problems

--- Scripts/camera_3d.gd
+++ Scripts/camera_3d.gd
@@ -1,21 +1,22 @@
 extends Camera3D
 @export var drag_speed := 0.05
-@export var zoom_speed := 10 
+@export var zoom_speed := 10
 @export var max_zoom := 1000.0
 @export var min_zoom := 50
-@export var grid_map:GridMap
-@onready var highlight: Node3D = $Highlight
+@export var grid_map: GridMap
 var dragging := false
 var last_mouse_pos := Vector2.ZERO
+@onready var highlight: Node3D = $Highlight
 
-func _process(delta: float) -> void:
+
+func _process(_delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
-	
+
 	#cast ray from camera from where the mouse is currently at
 	var from = self.project_ray_origin(mouse_pos)
-	var to = from + self.project_ray_normal(mouse_pos)* 1000
-	
-	var space_state= get_world_3d().direct_space_state
+	var to = from + self.project_ray_normal(mouse_pos) * 1000
+
+	var space_state = get_world_3d().direct_space_state
 	var query = PhysicsRayQueryParameters3D.create(from, to)
 	#query.exclude = [self] # exclude self to avoid self-intersection
 	query.collision_mask = 2
@@ -24,30 +25,25 @@ func _process(delta: float) -> void:
 	#print(result)
 	if result:
 		var pos = result.position
-		var cell = grid_map.local_to_map(pos) # conver mouse global pos to a cell position
+		var cell = grid_map.local_to_map(pos)  # conver mouse global pos to a cell position
 		var cell_center = grid_map.map_to_local(cell) + Vector3(0, 1, 0)  # slightly above
-		
-		var local_pos = grid_map.map_to_local(cell) # cell position to a local pos ()
+
+		var local_pos = grid_map.map_to_local(cell)  # cell position to a local pos ()
 		var global_pos = grid_map.to_global(local_pos)
 		#var tile_transform = grid_map.get_cell_item_transform(cell.x, cell.y, cell.z)
-		highlight.global_transform = Transform3D(
-			grid_map.global_basis,
-			global_pos
-		)
+		highlight.global_transform = Transform3D(grid_map.global_basis, global_pos)
 		#highlight.scale.x = grid_map.cell_size.x
 		#highlight.scale.z = grid_map.cell_size.z
 		#highlight.scale.y = 1
-		highlight.visible =true
-	
-	
-	pass
+		highlight.visible = true
+
 
 func _input(event):
 	#if event.is_action_pressed("left_click") :
-		#print("pressing something")
-		#var global_pos = get_viewport().get_mouse_position()
-		#var cell_pos = grid_map.local_to_map(global_pos)
-		#print("Clicked cell: ", cell_pos)
+	#print("pressing something")
+	#var global_pos = get_viewport().get_mouse_position()
+	#var cell_pos = grid_map.local_to_map(global_pos)
+	#print("Clicked cell: ", cell_pos)
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT:
 			dragging = event.pressed
--- current_day.gd
+++ current_day.gd
@@ -2,17 +2,16 @@ extends Label
 
 var currentday: int = 0
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	text = "Current Day: 0"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
 
 
 func _on_next_day_pressed() -> void:
-	currentday+=1
-	text = "Current Day: "+str(currentday)
-	pass # Replace with function body.
+	currentday += 1
+	text = "Current Day: " + str(currentday)
