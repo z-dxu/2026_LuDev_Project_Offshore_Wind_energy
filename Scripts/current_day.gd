1ece37f5e0c985b9c27f2a3f1e5a5f8081c53dc8
JakeSnake55 <76699554+JakeSnake55@users.noreply.github.com>
2026-03-19 15:50:02 +0000

    style: auto-format GDScript via gdformat [skip ci]

--- Scripts/current_day.gd
+++ Scripts/current_day.gd
@@ -8,9 +8,6 @@ func _ready() -> void:
 	text = "Current Day: 0"
 
 
-
-
-
 func _on_next_day_pressed() -> void:
 	currentday += 1
 	text = "Current Day: " + str(currentday)
--- Scripts/follow_mouse_pos.gd
+++ Scripts/follow_mouse_pos.gd
@@ -3,23 +3,19 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 
-
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click") :
-		get_child(0).visible = true;
+	if event.is_action_pressed("left_click"):
+		get_child(0).visible = true
 	if event is InputEventMouseMotion:
-		if(event.relative.is_zero_approx() == false):
-			get_child(0).visible = false;
+		if event.relative.is_zero_approx() == false:
+			get_child(0).visible = false
 
-		
-	
+
 func _process(_delta: float) -> void:
-	position = get_global_mouse_position();
-	
-	
+	position = get_global_mouse_position()
