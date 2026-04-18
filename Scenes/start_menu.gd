bbe488d87b13f40a35c09d8d08c4f268e7667f29
Huss-Alamodi <dr.yousefooo@gmail.com>
2026-04-18 13:24:49 +0200

    Updated Ui with a nicer look

--- Scenes/start_menu.gd
+++ Scenes/start_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/windmill_park.tscn")
+	get_tree().change_scene_to_file("res://Scenes/map.tscn")
 
 
 func _on_settings_pressed() -> void:
