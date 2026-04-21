1d638f07df6d2a197f25bdfa92d6aead7382b997
Huss-Alamodi <dr.yousefooo@gmail.com>
2026-04-21 23:47:26 +0200

    added a headless clause in scene transition script

--- Scripts/scene_transition.gd
+++ Scripts/scene_transition.gd
@@ -10,9 +10,11 @@ func change_scene(target: String) -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass  # Replace with function body.
+	#If running in cli no need to load graphical stuff
+	if DisplayServer.get_name() == "headless":
+		return
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process() -> void:
+func _process(_delta: float) -> void:
 	pass
